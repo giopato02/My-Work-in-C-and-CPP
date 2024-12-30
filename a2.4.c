3e268fbd29ebd82214eba29bae1294a568a985a0
Giorgi Pataridze <gpataridze@constructor.university>
2024-12-30 15:29:15 +0100

    Create a2.4.c

---
+++ a2.4.c
@@ -0,0 +1,28 @@
+/*
+CH-230-A
+a2 p4.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+#include <stdio.h>
+
+int main() {
+	
+	float a=0, b=0, h=0; // Declare and initialize variables
+	
+	// Input values for a, b, and h
+	printf("input the value a: ", a);
+	scanf("%f", &a);
+	printf("input the value b: ", b);
+	scanf(" %f", &b);
+	printf("input the value h: ", h);
+	scanf(" %f", &h);
+
+	// Calculate and print areas
+	printf("square area=%f\n", a*a);
+	printf("rectangle area=%f\n", a*b);
+	printf("triangle area=%f\n", (a*h)/2);
+	printf("trapezoid area=%f\n", ((a+b)/2)*h);
+
+	return 0;
+}
