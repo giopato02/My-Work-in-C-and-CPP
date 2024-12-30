a95e10611a117e5047e632bcbe7f1bfa51e847e4
Giorgi Pataridze <gpataridze@constructor.university>
2024-12-30 15:34:10 +0100

    Update a2.2.c

--- a2.2.c
+++ a2.2.c
@@ -4,19 +4,21 @@ a2 p2.[c]
 Giorgi Pataridze
 gpataridze@constructor.university
 */
+// Decimal, octal and hexadecimal representations of a character
 #include <stdio.h>
 
-int main() {
-	
-	char let1; // Declare a character variable
-	
-	scanf("%c", &let1); //input a character
+int main()
+{
 
-	//print the character in different notations
+	char let1; // Declare a character variable
+
+	scanf("%c", &let1); // input a character
+
+	// print the character in different notations
 	printf("character=%c\n", let1);
 	printf("decimal=%d\n", let1);
 	printf("octal=%o\n", let1);
 	printf("hexadecimal=%X\n", let1);
- 
+
 	return 0;
 }
