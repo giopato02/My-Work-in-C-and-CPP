903c21d9dcc928291494e166e4c745d58efd3fa5
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-02 16:32:11 +0100

    Create a2.7.c

---
+++ a2.7.c
@@ -0,0 +1,15 @@
+/*
+CH-230-A
+a2 p7.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+#include <stdio.h>
+int main(){
+    int i = 8;
+    while (i >= 4){
+        printf("i is %d\n", i--);
+    }
+    printf("That's it.\n");
+return 0;
+}
