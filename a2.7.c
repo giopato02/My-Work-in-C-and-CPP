1987f5b99ab5cf62f106d02de10007d5b2233859
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-02 16:32:52 +0100

    Update a2.7.c

--- a2.7.c
+++ a2.7.c
@@ -15,5 +15,17 @@ return 0;
 }
 /*
 Second way to solve this problem
+#include <stdio.h>
+int main()
+{
+    int i = 8;
+    while (i >= 4)
+    {
+        printf("i is %d\n", i);
+        i--;
+    }
+    printf("That's it.\n");
 
+    return 0;
+}
 */
