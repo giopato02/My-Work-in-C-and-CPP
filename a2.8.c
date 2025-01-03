b8ddbec0a66fddb3e96bcd99d5cc39c7535ef154
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-03 12:06:37 +0400

    Update a2.8.c

--- a2.8.c
+++ a2.8.c
@@ -19,3 +19,23 @@ int main(){
     
 return 0;
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    if(n % 2 == 0 && n % 7 == 0)
+    {
+        printf("The number is divisible by 2 and 7\n");
+    }
+    else{
+        printf("The number is NOT divisible by 2 and 7\n");
+    }
+
+    return 0;
+}
+*/
