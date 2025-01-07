756d6ff3a26ffacee85b2fde7ec616c38e87f26e
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-07 22:13:29 +0400

    Update a3.2.c

---
+++ A3/a3.2.c
@@ -0,0 +1,28 @@
+/*
+CH-230-A
+a3 p2.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     char ch;  // declares character variable
+//     int n, i; // declares int variables
+
+//     // input code
+//     scanf("%c", &ch);
+//     scanf("%d", &n);
+
+//     printf("%c", ch);
+
+//     // for loop function
+//     for (i = 1; i <= n; i++)
+//     {
+//         printf(", %c-%d", ch, i);
+//     }
+
+//     return 0;
+// }
