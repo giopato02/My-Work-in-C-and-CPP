8f42a63cbaaab63b240199a26c626ae78dbb780e
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-08 18:49:48 +0400

    Update a3.3.c

---
+++ A3/a3.3.c
@@ -0,0 +1,27 @@
+/*
+CH-230-A
+a3 p3.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+// #include <stdio.h>
+
+// // Function that converts cm to km
+// float convert(int cm)
+// {
+//     return cm * 0.00001;
+// }
+
+// int main()
+// {
+//     int cm; // Declares cm
+
+//     // Input cm
+//     printf("Enter a number: ");
+//     scanf("%d", &cm);
+
+//     // Convert and print in km
+//     printf("result of conversion: %f", convert(cm));
+
+//     return 0;
+// }
