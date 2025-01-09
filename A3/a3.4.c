955e3e7a289f621ee9d60d241f05f03586457c3a
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-09 16:54:08 +0400

    Update a3.4.c

---
+++ A3/a3.4.c
@@ -0,0 +1,32 @@
+/*
+CH-230-A
+a3 p4.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+// #include <stdio.h>
+
+// int position(char str[], char c)
+// {
+//     int idx;
+//     /* Loop until end of string */
+//     for (idx = 0; str[idx] != c && str[idx] != '\0'; ++idx)
+//     { // The problem was that "return idx;" was written in curly brackets where it was declared that idx = 0.
+//     }
+
+//     return idx;
+
+//     /* do nothing */
+// }
+
+// int main()
+// {
+//     char line[80];
+//     while (1)
+//     {
+//         printf("Enter string:\n");
+//         fgets(line, sizeof(line), stdin);
+//         printf("The first occurrence of 'g' is: %d\n", position(line, 'g'));
+//     }
+// }
+
