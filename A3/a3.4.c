be926985b8787d3e9f2f296749f74e64c4fe67ba
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-09 16:54:23 +0400

    Update a3.4.c

--- A3/a3.4.c
+++ A3/a3.4.c
@@ -30,3 +30,22 @@ gpataridze@constructor.university
 //     }
 // }
 
+
+#include <stdio.h>
+
+int position(char str[], char c)
+{
+    int idx;
+    for (idx = 0; str[idx] != c && str[idx] != '\0'; idx++)
+    {}
+
+    return idx;
+ }
+ int main() {
+    char line[80];
+    while (1) {
+        printf("Enter string:\n");
+        fgets(line, sizeof(line), stdin);
+        printf("The first occurrence of 'g' is: %d\n", position(line, 'g'));
+    }
+ }
