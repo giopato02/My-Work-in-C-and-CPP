f7b353956f252e9db0a64e317331db76070fb907
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-11 11:33:35 +0400

    Update a3.6.c

---
+++ A3/a3.6.c
@@ -0,0 +1,24 @@
+/*
+CH-230-A
+a3 p6.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+// #include <stdio.h>
+
+// float to_pounds(int kg, int g)
+// {
+//     return (kg + g / 1000.0) * 2.2;
+// }
+
+// int main()
+// {
+
+//     float kg, g;
+
+//     scanf("%f%f", &kg, &g);
+
+//     printf("Result of conversion: %f", to_pounds(kg, g));
+
+//     return 0;
+// }
