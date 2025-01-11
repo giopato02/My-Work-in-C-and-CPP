ea387ce611c2ac1b57f1d45f240eb751ffecdd81
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-11 11:33:56 +0400

    Update a3.6.c

--- A3/a3.6.c
+++ A3/a3.6.c
@@ -22,3 +22,25 @@ gpataridze@constructor.university
 
 //     return 0;
 // }
+
+#include <stdio.h>
+
+float to_pounds(int kg, int g)
+{
+    float pounds;
+    pounds = (kg + (g * 0.001)) * 2.2;
+    return pounds;
+}
+
+int main()
+{
+    int kg, g;
+    printf("Enter kgs: ");
+    scanf("%d", &kg);
+    printf("Enter grams: ");
+    scanf("%d", &g);
+
+    printf("Result of Conversion to pounds: %f", to_pounds(kg, g));
+
+    return 0;
+}
