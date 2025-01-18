0aa8a17e12872eaf18c32d738a90b848b007c1a3
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-18 15:49:37 +0400

    Update a4.2.c

--- A4/a4.2.c
+++ A4/a4.2.c
@@ -24,3 +24,30 @@ gpataridze@constructor.university
 
 //     return 0;
 // }
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[50];
+
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    int length = strlen(str);
+
+    for(int i = 0; i < length; i++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%c\n", str[i]);
+        }
+        else
+        {
+            printf(" %c\n", str[i]);
+        }
+    }
+
+    return 0;
+}
