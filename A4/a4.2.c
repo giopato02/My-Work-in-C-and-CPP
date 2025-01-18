7d5b5eb91f69da72a450089a508c45af1a46b901
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-18 15:49:02 +0400

    Update a4.2.c

---
+++ A4/a4.2.c
@@ -0,0 +1,26 @@
+/*
+CH-230-A
+a4 p2.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+// #include <stdio.h>
+
+// int main()
+// {
+//     char string[50]; // Array that holds 50 characters;
+//     int i;
+
+//     // Input code
+//     printf("Enter a word(s): ");
+//     fgets(string, sizeof(string), stdin);
+
+//     for (i = 0; string[i] != '\0'; i++)
+//     {
+//         if (i % 2 == 1)
+//             printf(" ");
+//         printf("%c\n", string[i]);
+//     }
+
+//     return 0;
+// }
