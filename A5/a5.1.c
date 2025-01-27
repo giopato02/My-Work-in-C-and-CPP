3917953ff7a4d2bce342b388be151457ecbe33ae
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-27 17:17:24 +0400

    Update a5.1.c

---
+++ A5/a5.1.c
@@ -0,0 +1,58 @@
+/*
+CH-230-A
+a5 p1.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     char ch;
+
+//     scanf("%d", &n);
+//     scanf(" %c", &ch);
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         for(int j = n; j > i; j--)
+//         {
+//             printf("%c", ch);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     char ch;
+
+//     scanf("%d", &n);
+//     scanf(" %c", &ch);
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         for(int j = 0; j <= i; j++)
+//         {
+//             printf("%c", ch);
+//         }
+//         printf("\n");
+//     }
+//     for(int i = 0; i < n; i++)
+//     {
+//         for(int j = n; j > i; j--)
+//         {
+//             printf("%c", ch);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
