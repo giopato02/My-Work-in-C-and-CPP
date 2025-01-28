74305b4442398a5530c393f66a4a26aa2a23ff0b
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-28 20:40:50 +0400

    Update a5.2.c

--- A5/a5.2.c
+++ A5/a5.2.c
@@ -40,3 +40,35 @@ gpataridze@constructor.university
 
 //     return 0;
 // }
+
+#include <stdio.h>
+
+void divby5(float arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = arr[i] / 5.0;
+    }
+}   
+
+int main()
+{
+    int n;
+    n = 6;
+    float arr[6] = {5.5, 6.5, 7.75, 8.0, 9.6, 10.36};
+
+    printf("Before:\n");
+    for(int i = 0; i < 6; i++)
+    {
+        printf("%.3f ", arr[i]);
+    }
+    printf("\n");
+
+    divby5(arr, n);
+    printf("After:\n");
+    for(int i = 0; i < 6; i++)
+    {
+        printf("%.3f ", arr[i]);
+    }
+    return 0;
+}
