00701234ba4edac970b1208253000b375adb13de
Giorgi Pataridze <gpataridze@constructor.university>
2025-01-28 20:40:30 +0400

    Update a5.2.c

---
+++ A5/a5.2.c
@@ -0,0 +1,42 @@
+/*
+CH-230-A
+a5 p2.[c]
+Giorgi Pataridze
+gpataridze@constructor.university
+*/
+// #include <stdio.h>
+
+// void divby5(float arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = arr[i] / 5.0; // divide each element by 5
+//     }
+// }
+
+// int main()
+// {
+//     float arr[] = {5.5, 6.5, 7.75, 8.0, 9.6, 10.36};
+//     int size = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
+
+//     // Print the array before division
+//     printf("Before:\n");
+//     for (int i = 0; i < size; i++)
+//     {
+//         printf("%.3f ", arr[i]);
+//     }
+//     printf("\n");
+
+//     // call the function
+//     divby5(arr, size);
+
+//     // print after division
+//     printf("After:\n");
+//     for (int i = 0; i < size; i++)
+//     {
+//         printf("%.3f ", arr[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
